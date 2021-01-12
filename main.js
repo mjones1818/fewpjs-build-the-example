cef3711e4d75c265d900c4db0e389c2e408b825c
mjones1818 <michael.jones0532@gmail.com>
2021-01-12 12:22:59 -0600

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+let articles = document.getElementsByClassName('media-post')
+let errorModal = document.querySelector('#modal')
+document.body.addEventListener('click', function(e){
+  if (e.target.classList.contains('like-glyph') && e.target.classList.length == 1) {
+    mimicServerCall()
+    .then(function(response) {
+      if (response == 'Pretend remote server notified of action!') {
+        e.target.innerText = FULL_HEART
+        e.target.classList.add('activated-heart') 
+      }
+      return response
+    })
+    .catch(function(error){
+      errorModal.lastElementChild.innerText = error
+      errorModal.classList.toggle('hidden')
+      setTimeout(function(){
+        errorModal.classList.toggle('hidden')
+      }, 5000)
+    })
+  } else if (e.target.classList.contains('activated-heart')) {
+    e.target.classList.toggle('activated-heart')
+    e.target.innerText = EMPTY_HEART
+  }
+})
 
 
 //------------------------------------------------------------------------------
